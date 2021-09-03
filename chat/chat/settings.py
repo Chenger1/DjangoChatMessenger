612c9ab7d5ef3f804a920117c5b2523d4b80aa4f
Chenger1 <exs2199@gmail.com>
2021-09-03 18:15:24 +0300

    Replace exposed secret key. Now it stores in .env file

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .config import KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+3lqv4i-kwilbzcuedhpz8m(^(*p0c$w6jv$j=d@s-+3+6pu_6'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
